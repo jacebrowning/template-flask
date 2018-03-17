7d7731dd159cc949c651d12c2e8317321ce1ef69
Jace Browning <jacebrowning@gmail.com>
2018-03-17 10:58:58 -0400

    Remove unsupported options

--- {{cookiecutter.project_name}}/tests/integration/test_api_root.py
+++ {{cookiecutter.project_name}}/tests/integration/test_api_root.py
@@ -1,17 +1,2 @@
-# pylint: disable=unused-variable,unused-argument,expression-not-assigned,redefined-outer-name
-
-import pytest
-
-
-def describe_root():
-
-    @pytest.fixture
-    def index():
-        return "/api/"
-
-    def describe_GET():
-
-        def it_always_returns_200(expect, client, index):
-            response = client.get(index)
-
-            expect(response.status_code) == 200
+def test_placeholder():
+    assert True
